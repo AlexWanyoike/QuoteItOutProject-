61647884c2ed5e9287e86deda953eee5786928ee
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-03-30 16:18:24 +0300

    Goals

--- Goals/src/app/goal-detail/goal-detail.component.ts
+++ Goals/src/app/goal-detail/goal-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, Input, OnInit } from '@angular/core';
+import { Goal } from './../goal';
 
 @Component({
   selector: 'app-goal-detail',
@@ -6,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./goal-detail.component.css']
 })
 export class GoalDetailComponent implements OnInit {
-
+  
+  @Input() goal: Goal;
   constructor() { }
 
   ngOnInit(): void {
