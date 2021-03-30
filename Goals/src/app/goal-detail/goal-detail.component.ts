8f2a911da59ac74beafdc08821fc0bf0c3a42524
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-03-30 18:49:40 +0300

    Toggle Input

--- Goals/src/app/goal-detail/goal-detail.component.ts
+++ Goals/src/app/goal-detail/goal-detail.component.ts
@@ -17,6 +17,10 @@ export class GoalDetailComponent implements OnInit {
   goalComplete(complete:boolean){
     this.isComplete.emit(complete);
   }
+  
+  goalDelete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
   constructor() { }
 
   ngOnInit() {
--- Goals/src/app/goal/goal.component.ts
+++ Goals/src/app/goal/goal.component.ts
@@ -27,6 +27,16 @@ export class GoalComponent implements OnInit {
     this.goals[index].showDescription = !this.goals[index].showDescription;
   }
 
+  deleteGoal(isComplete: any, index: number){
+    if (isComplete) {
+      let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
+
+      if (toDelete){
+        this.goals.splice(index,1)
+      }
+    }
+  }
+
   constructor() { }
 
   ngOnInit() {
