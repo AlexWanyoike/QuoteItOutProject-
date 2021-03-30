8067262defc3fa0548162a4cac61e0e33ae3a2d4
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-03-30 15:30:12 +0300

    Initial Start

--- Goals/src/main.ts
+++ Goals/src/main.ts
@@ -4,18 +4,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import { Car } from './car';
-import { Engine } from './engine';
-import { Wheels } from './wheels';
-
-function main(){
-  let car = new Car();
-
-  car.startEngine();
-}
-
-main();
-
 if (environment.production) {
   enableProdMode();
 }
