7142be1f2477ae140bed25b4fdccf4b47fe591b7
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-03-30 13:52:52 +0300

    Initial Start

---
+++ weekoneangular/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { BlockformComponent } from '../app/blockform/blockform.component';
+import { NgModule } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  
+})
+export class AppComponent {
+  title = 'Week One Angular Project';
+  
+}
