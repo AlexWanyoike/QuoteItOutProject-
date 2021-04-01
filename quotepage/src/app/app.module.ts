99372a1b41c5c962485b08b28ecde94f37d7bc9b
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-04-01 06:59:24 +0300

    quote, quoteform, quotedetails,quoteclass

--- quotepage/src/app/app.module.ts
+++ quotepage/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteformComponent } from './quoteform/quoteform.component';
+import { QuotedetailsComponent } from './quotedetails/quotedetails.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    QuoteformComponent,
+    QuotedetailsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
