4470abbaf838b56e0fda49b2009dd88306abddd9
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-04-04 22:58:55 +0300

    CSS Styles on Github

--- githubapi/src/app/github-repository/github-repository.component.ts
+++ githubapi/src/app/github-repository/github-repository.component.ts
@@ -8,27 +8,9 @@ import { Component, Input, OnInit } from '@angular/core';
 export class GithubRepositoryComponent implements OnInit {
 
   @Input() githubRepos:any[] | undefined;
-  repos: Repos[] =[];
-  reponame: any | undefined;
-
-  constructor(public rs: RestService) { }
-
-  ngOnInit(): void {
-    this.rs.getRepos().subscribe(((reponse: any[])=>{
-      this.repos =reponse;
-    }))
-  }
-
-  Search(){
-    if(this.reponame ==""){
-      this.ngOnInit()
-    }else{
-      this.repos = this.repos.filter(res =>{
-        return res.reponame.toLocaleLowerCase().match(this.reponame.toLocaleLowerCase());
-      })
-    }
-  }
-
   
+  ngOnInit(){
+    
+  }
 
 }
