64f195eeaca949053fff2dbd56a8c61799b8c180
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-04-04 10:56:02 +0300

    Components Added

--- githubapi/src/app/app.module.ts
+++ githubapi/src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubapipipePipe } from './githubapipipe.pipe';
+import { FormsModule } from '@angular/forms';
+import { GithubNavbarComponent } from './github-navbar/github-navbar.component';
+import { GithubProfileCardComponent } from './github-profile-card/github-profile-card.component';
+import { GithubProfileComponent } from './github-profile/github-profile.component';
+import { GithubRepositoryComponent } from './github-repository/github-repository.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    GithubapipipePipe
+    GithubapipipePipe,
+    GithubNavbarComponent,
+    GithubProfileCardComponent,
+    GithubProfileComponent,
+    GithubRepositoryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
