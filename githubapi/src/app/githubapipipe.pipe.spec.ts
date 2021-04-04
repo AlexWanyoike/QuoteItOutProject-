63a246269483064fb759933f882cd75d1b3bb4c1
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-04-04 10:51:22 +0300

    Pipe, Class and Bootstrap Added

---
+++ githubapi/src/app/githubapipipe.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { GithubapipipePipe } from './githubapipipe.pipe';
+
+describe('GithubapipipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new GithubapipipePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
