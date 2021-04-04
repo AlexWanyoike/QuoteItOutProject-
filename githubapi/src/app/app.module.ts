26942fb2b4be008e94bb11f310ffe2e5cced7ebd
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-04-04 12:59:33 +0300

    Navbar Added and Styled

--- githubapi/src/app/app.module.ts
+++ githubapi/src/app/app.module.ts
@@ -8,7 +8,8 @@ import { FormsModule } from '@angular/forms';
 import { GithubNavbarComponent } from './github-navbar/github-navbar.component';
 import { GithubProfileCardComponent } from './github-profile-card/github-profile-card.component';
 import { GithubProfileComponent } from './github-profile/github-profile.component';
-import { GithubRepositoryComponent } from './github-repository/github-repository.component'
+import { GithubRepositoryComponent } from './github-repository/github-repository.component';
+import { GithubAppComponent } from './github-app/github-app.component'
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { GithubRepositoryComponent } from './github-repository/github-repository
     GithubNavbarComponent,
     GithubProfileCardComponent,
     GithubProfileComponent,
-    GithubRepositoryComponent
+    GithubRepositoryComponent,
+    GithubAppComponent
   ],
   imports: [
     BrowserModule,
